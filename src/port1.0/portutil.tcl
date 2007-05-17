cooljeanius/MacPorts-fork

commit 533167c52f6297cb7f421ed97623df87fad6bc04

Add -E flag to reinplace
git-svn-id: https://svn.macports.org/repository/macports/trunk/base@25251 d073be05-634f-4543-b044-5fe20cf6d1d6

src/port1.0/portutil.tcl
@@ -661,13 +661,34 @@ proc ldelete {list value} {
 
 # reinplace
 # Provides "sed in place" functionality
-proc reinplace {pattern args}  {
-    if {$args == ""} {
-    	ui_error "reinplace: no value given for parameter \"file\""
-	return -code error "no value given for parameter \"file\" to \"reinplace\"" 
+proc reinplace {args}  {
+    set extended 0
+    while 1 {
+        set arg [lindex $args 0]
+        if {[string first - $arg] != -1} {
+            set args [lrange $args 1 end]
+            switch [string range $arg 1 end] {
+                E {
+                    set extended 1
+                }
+                - {
+                    break
+                }
+                default {
+                    error "reinplace: unknown flag '-$arg'"
+                }
+            }
+        } else {
+            break
+        }
     }
+    if {[llength $args] < 2} {
+        error "reinplace ?-E? pattern file ..."
+    }
+    set pattern [lindex $args 0]
+    set files [lrange $args 1 end]
     
-    foreach file $args {
+    foreach file $files {
 	if {[catch {set tmpfile [mkstemp "/tmp/[file tail $file].sed.XXXXXXXX"]} error]} {
 		global errorInfo
 		ui_debug "$errorInfo"
@@ -680,7 +701,12 @@ proc reinplace {pattern args}  {
 	    set tmpfile [lindex $tmpfile 1]
 	}
 	
-	if {[catch {exec sed $pattern < $file >@ $tmpfd} error]} {
+	set cmdline sed
+	if {$extended} {
+	    lappend cmdline -E
+	}
+	set cmdline [concat $cmdline [list $pattern < $file >@ $tmpfd]]
+	if {[catch {eval exec $cmdline} error]} {
 		global errorInfo
 		ui_debug "$errorInfo"
 	    ui_error "reinplace: $error"

doc/portfile.7
@@ -1591,8 +1591,17 @@ with adduser:
 .Dl adduser foo gid=[existsgroup foo]
 .It Ic nextgid
 Returns the highest used gid plus one.
-.It Ic reinplace Ar regex Ar filename
-Provide in-place sed like editing of a file.
+.It Xo
+.Ic reinplace
+.Op Fl E
+.Ar regex
+.Ar
+.Xc
+Provide in-place
+.Xr sed 1
+like editing of a file.
+The -E flag does the same thing as in
+.Xr sed 1
 .br
 .Sy Example:
 .Dl reinplace \*qs|/usr/local|${prefix}|g\*q doc/manpage.1

commit 7ba1055f259a4092fd5a613ce515ad7e0540e034

Fix reinplace, which I broke in r25251
git-svn-id: https://svn.macports.org/repository/macports/trunk/base@25285 d073be05-634f-4543-b044-5fe20cf6d1d6

src/port1.0/portutil.tcl
@@ -665,7 +665,7 @@ proc reinplace {args}  {
     set extended 0
     while 1 {
         set arg [lindex $args 0]
-        if {[string first - $arg] != -1} {
+        if {[string index $arg 0] eq "-"} {
             set args [lrange $args 1 end]
             switch [string range $arg 1 end] {
                 E {
@@ -675,7 +675,7 @@ proc reinplace {args}  {
                     break
                 }
                 default {
-                    error "reinplace: unknown flag '-$arg'"
+                    error "reinplace: unknown flag '$arg'"
                 }
             }
         } else {