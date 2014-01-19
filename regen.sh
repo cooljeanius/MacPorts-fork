af66b606194a26ac6795f2e7d7bcf353c1e23fef
Eric Gallager <egall@gwmail.gwu.edu>
2014-01-18 19:42:13 -0500

    minor tweaks resulting from cloning this repo onto a new drive

--- regen.sh
+++ regen.sh
@@ -5,5 +5,8 @@ echo "You would probably be better off just running \"autoreconf\" with your fav
 
 set -ex
 
-autoconf --warnings=all --force
+autoconf --warnings=all,no-obsolete --force
 autoheader --warnings=all --force
+
+test -e src/config.h.in~ && rm -f src/config.h.in~
+test -d autom4te.cache && rm -rf autom4te.cache
